0c3bd47c9da8bd62952226726e50faaad3610998
AlexsJones <alexsimonjones@gmail.com>
2012-11-15 20:56:57 +0000

    added a very simple sql interface

---
+++ src/sql_interface.h
@@ -0,0 +1,12 @@
+#ifndef __SQL_INTERFACE_H__
+#define __SQL_INTERFACE_H__
+#include <mysql/mysql.h>
+///function pointer callback
+typedef void (*sql_callback)(MYSQL_RES*);
+///Warning: SQL will need to be included into project as dependency
+///sql_interface_setup sets the HOST USERNAME PASSWOD
+int sql_interface_setup(char *host,char *username, char *password);
+///sql_query takes query string and callback
+int sql_query(char *query,void (*sql_callback)(MYSQL_RES*));
+
+#endif
--- test/testbay.c
+++ test/testbay.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include "../src/list.h"
+#include "../src/sql_interface.h"
 
+void _callback(MYSQL_RES *res)
+{
+	
+}
 int main(int argc, char **argv) {
 
-	struct list* l = list_make();
+	sql_interface_setup("localhost","jonesax","trinity");
+	sql_callback c = &_callback;
 	
-	list_add(l,"hello herp dp aoimaoi oimab \n aoimvao \n");
-	list_add(l,"hello");
-	list_add(l,"up");
-	list_add(l,"down");
-	list_add(l,"bye");
-
+	sql_query("use test;",c);
     return 0;
 }
