c5903bf999cf2116d19d40cb4a286a8bf40a0412
AlexsJones <alexsimonjones@gmail.com>
2012-12-21 18:07:36 +0000

    added a jnx_file for reading from files, will add more io

--- src/jnxfile.h
+++ src/jnxfile.h
@@ -1,8 +1,7 @@
 #ifndef __JNXFILE_H__
 #define __JNXFILE_H__
 
-///Returns a callocd string with the file data, you will assume responsibility 
-///for freeing 
+///Returns a callocd string with the file data, you will assume responsibility for freeing 
 char* jnx_file_tostr(char*path);
 
 #endif
