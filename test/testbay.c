2068b32113523ef8f16d1bd5d7118f4dc20d1fd6
AlexsJones <alexsimonjones@gmail.com>
2013-01-09 11:30:33 +0000

    added key value pair file puller

--- test/testbay.c
+++ test/testbay.c
@@ -7,7 +7,16 @@
 
 int main (int argc, char** argv)
 {
-	jnx_file_recursive_delete("test");
+	struct jnx_file_kvp_node *head = jnx_file_read_keyvaluepairs("config.conf");
+	struct jnx_file_kvp_node *node = head;
+	
+	printf("Got node\n");
+	
+	while(node != NULL)
+	{
+		printf("Key %s Value %s\n",node->key,node->value);
+		node = node->next;
+	}
 	
     return 0;
 }
