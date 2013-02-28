87900a836740013ba92b1920dd7bfce7e12b8ece
alexsimonjones@gmail.com <alexsimonjones@gmail.com>
2013-02-28 07:40:56 +0000

    fails the installation based on test output

--- build_and_install.sh
+++ build_and_install.sh
@@ -2,4 +2,14 @@
 
 ./configure
 make
+cd test
+./run_tests
+result=$?
+if [ ! $result -eq 0 ]; then
+echo "Error during testing: Aborting installation"
+cd ../ 
+exit 1
+fi
+cd ../
 sudo make install
+echo "Installed succesfully"
