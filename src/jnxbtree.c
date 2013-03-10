ebc2f7b57ba2d25ebfb3ca6abd4b09bb8de1a50c
draganglumac <dragan.glumac@gmail.com>
2013-03-10 14:38:07 +0000

    Regrouped the functions and decided to split any full nodes that are encountered on the way down before inserting the record.

--- src/jnxbtree.c
+++ src/jnxbtree.c
@@ -33,16 +33,45 @@ jnx_B_tree_node *new_node(int order, int is_leaf)
     return node;
 }
 
-int find_index_for_record(jnx_B_tree *tree, jnx_B_tree_node *node, record *r)
+int find_index_of_child_for_key(jnx_B_tree_node *node, void *key)
 {
-    // Stub
+    // ToDo: Stub
+
+    // Records are ordered so to make search efficient
+    // (especially for arrays with lots of records)
+    // we use binary chop, which is O(lg n) time.
     return 0;
 }
 
-int shift_right(jnx_B_tree *tree, jnx_B_tree * node, int start_index)
+void move_contents_from_index(jnx_B_tree_node *source, jnx_B_tree_node *target, int index)
 {
-	// Stub
-	return 0;
+    // Copy the RHS half of the records and children to new node
+    memcpy((void *) target->records, 
+           (const void*) (source->records + index),
+           source->count - index - 1);
+    memcpy((void *) target->children,
+           (const void*) (source->children + index + 1),
+           source->count - index);
+    
+    // Zero out the old records and children in the old node
+    bzero((void *) (source->records + index), source->count - index - 1);
+    bzero((void *) (source->children + index + 1), source->count - index);
+
+    // Adjust record counts in the nodes
+    source->count = source->count - index - 1;
+    target->count = source->count;
+}
+
+void shift_right_from_index(jnx_B_tree_node *node, int index)
+{
+    // Shift records to the right of i by one position
+    memcpy((void *) (node->records + index),
+           (const void*) (node->records + index + 1),
+           node->count - index + 1);
+    // Shift children to the rigth of i + 1 by one position
+    memcpy((void *) (node->children + index + 1),
+           (const void*) (node->children + index + 2),
+           node->count - index + 1);
 }
 
 int is_node_full(jnx_B_tree *tree, jnx_B_tree_node *node)
@@ -55,12 +84,6 @@ int is_node_full(jnx_B_tree *tree, jnx_B_tree_node *node)
 	return 0;
 }
 
-jnx_B_tree_node *find_subtree_for_record(jnx_B_tree *tree, jnx_B_tree_node *node, record *r)
-{
-	int i = find_index_for_record(tree, node, r);
-	return node->children[i];
-}
-
 void split_child_at_index(jnx_B_tree *tree, jnx_B_tree_node *node, int child_index)
 {
     int tree_order = tree->order;
@@ -68,58 +91,68 @@ void split_child_at_index(jnx_B_tree *tree, jnx_B_tree_node *node, int child_ind
     jnx_B_tree_node *temp = node->children[child_index];
     jnx_B_tree_node *sibling = new_node(tree_order, temp->is_leaf);
     
-    // Copy the RHS half of the records and children to new node
-    memcpy((void *) sibling->records, 
-           (const void*) (temp->records + tree_order),
-           tree_order - 1);
-    memcpy((void *) sibling->children,
-           (const void*) (temp->children + tree_order + 1),
-           tree_order);
-    // Zero out the old records and children in the old node
-    bzero((void *) (temp->records + tree_order), tree_order);
-    bzero((void *) (temp->children + tree_order + 1), tree_order);
-
-    // Adjust record counts in the nodes
-    temp->count = tree_order - 1;
-    sibling->count = tree_order - 1;
+    move_contents_from_index(temp, sibling, tree_order);
 
     // Now rearrange "node" to fit the new record and its children
     record *middle = temp->records[tree_order - 1];
-    int i = find_index_for_record(tree, node, middle);
-    // shift records to the right of i by one position
-    memcpy((void *) (node->records + i),
-           (const void*) (node->records + i + 1),
-           node->count - i + 1);
-    // shift children to the rigth of i + 1 by one position
-    memcpy((void *) (node->children + i + 1),
-           (const void*) (node->children + i + 2),
-           node->count - i + 1);
+    int i = find_index_of_child_for_key(node, middle->key);
+
+    shift_right_from_index(node, i);
+    
     // move the middle record up
     node->records[i] = middle;
     // attach newly created sibling
     node->children[i + i] = sibling;
+    // finally, increment the record count in the current node
+    node->count++;
 }
 
-void insert_into_tree_node(jnx_B_tree *tree, jnx_B_tree_node *node, jnx_B_tree_node *parent, record *r)
+void add_record_to_non_full_node(jnx_B_tree_node *node, record *r)
 {
-	if ( ! node->is_leaf )
-	{
-		int i = find_child_index_for_record(tree, node, r);
-    	insert_into_child_at_index(tree, node, i, r);
-	}
-    
-	if ( is_node_full(tree, node) )
-    {
-        jnx_B_tree_node *temp = node;
-        node = new_node(tree->order, 0);
-        node->children[0] = temp;
+    // ToDo: Stub
 
-        split_child_at_index(tree, tree->root, 0);
-    }
-	else
-	{
-		
-	}
+    int size = node->count;
+}
+
+/* 
+ * To guarantee that leaf node is never full, we split any full
+ * node on the way down the tree, find the appropriate subtree
+ * to traverse, and insert the node into the subtree.
+ *
+ * The only special case is if the root is a leaf node. We handle
+ * this in jnx_B_tree_add API function.
+ */
+void insert_into_tree_at_node(jnx_B_tree *tree, jnx_B_tree_node *node, record *r)
+{
+   if ( node->is_leaf )
+   {
+       add_record_to_non_full_node(node, r);
+       return;
+   }
+   
+   int i = find_index_of_child_for_key(node, r->key);
+   
+   if ( tree->compare_function(node->records[i]->key, r->key) == 0 )
+   {
+       // Same key so just replace the old record with the new one
+       free(node->records[i]);
+       node->records[i] = r;
+       return;
+   }
+
+   if ( is_node_full(tree, node->children[i]) )
+   {
+       split_child_at_index(tree, node, i);
+       
+       if ( tree->compare_function(node->records[i]->key, r->key) < 0 )
+       {
+           // We're going to the right of the record that moved up
+           i++;
+       }
+   }
+
+   // Recurse down into the appropriate subtree
+   insert_into_tree_at_node(tree, node->children[i], r);
 }
 
 /*
@@ -145,40 +178,18 @@ void jnx_B_tree_add(jnx_B_tree *tree, void *key, void *value)
     r->key = key;
     r->value = value;
 
-	if ( tree->root->is_leaf )
-	{
-		if ( is_node_full(tree->root) )
-		{
-			jnx_B_tree_node *temp = tree->root;
-			tree->root = new_node(tree->order, 0);
-			tree->root->children[0] = temp;
+    if ( is_node_full(tree, tree->root) )
+    {
+        jnx_B_tree_node *temp = tree->root;
+        tree->root = new_node(tree->order, 0);
+        tree->root->children[0] = temp;
 
-			split_child_at_index(tree, tree->root, 0);
+        split_child_at_index(tree, tree->root, 0);
 
-			if ( r->key < tree->root->records[0]->key )
-			{
-				add_record_to_non_full_node(tree->root->children[0], r);
-			}
-			else if ( r->key > tree->root->records[0]->key )
-			{
-				add_record_to_non_full_node(tree->root->children[1], r);
-			}
-			else
-			{
-				// Update the value - same key
-				tree->root->records[0]->value = r->value;
-			}
-		}
-		else
-		{
-			add_record_to_non_full_node(tree->root, r);
-		}
-	}
-	else
-	{
-		int i = find_index_of_child_for_key(tree, tree->root, r->key);
-		insert_record_into_child_at_index(tree, tree->root, i, r);
-	}
+        tree->root->count = 1;
+    }
+
+    insert_into_tree_at_node(tree, tree->root, r);
 }
 
 void *jnx_B_tree_lookup(jnx_B_tree *tree, void *key)
