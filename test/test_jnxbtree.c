5db50fae401b549d1796427c553fca96f05e3187
draganglumac <dragan.glumac@gmail.com>
2013-03-13 22:50:21 +0000

    Skeleton for the B-tree test.

--- test/test_jnxbtree.c
+++ test/test_jnxbtree.c
@@ -16,5 +16,32 @@
  * =====================================================================================
  */
 #include <stdlib.h>
+#include <assert.h>
+
 #include "../src/jnxbtree.h"
 
+int compare_ints(void *first, void *second)
+{
+    int *n1 = (int *) first;
+    int *n2 = (int *) second;
+
+    if ( *n1 == *n2 )
+    {
+        return 0;
+    }
+    else if ( *n1 < *n2 )
+    {
+        return -1;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int main()
+{
+    jnx_B_tree *tree = jnx_B_tree_init(3, compare_ints);
+
+    return 0;
+}
