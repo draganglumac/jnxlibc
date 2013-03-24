aa8e44a5811bc6ea5b0783683666230d3a8dd698
draganglumac <dragan.glumac@gmail.com>
2013-03-24 08:45:15 +0000

    Changed Doxyfile to only look at .h files, and enabled man_links so that function names are indexed as well. This was one of the main reasons why I removed *.c from the FILE_PATTERNS as the links will actually show even the private functions defined in .c file. I thought it was safe to do it as our API should be contained in .h files anyway.@

--- src/jnxbtree.h
+++ src/jnxbtree.h
@@ -16,12 +16,16 @@
  * =====================================================================================
  */
 
+/** @file jnxbtree.h
+ * @brief The API for jnxlibc's implementation of B-trees.
+ */
+
 #ifndef __JNX_B_TREE__
 #define __JNX_B_TREE__
 
 
 /**
- * \brief Internal representation of a (key, value) pair.
+ * @brief Internal representation of a (key, value) pair.
  */
 typedef struct {
     void *key; /**< The key for the record. */
@@ -30,7 +34,7 @@ typedef struct {
 
 
 /**
- * \brief Type for the callback function to determine the order of two keys.
+ * @brief Type for the callback function to determine the order of two keys.
  *
  * Callback function that the user needs to supply and which compares two keys,
  * and effectivelly two records as keys and values live together in a node record.
@@ -47,7 +51,7 @@ typedef int (*compare_keys) (void*, void*);
 
 
 /**
- * \brief Internal representation of a node in a B-tree.
+ * @brief Internal representation of a node in a B-tree.
  */
 typedef struct B_tree_node {
     int count; /**< How many records are in the node. */
@@ -58,7 +62,7 @@ typedef struct B_tree_node {
 
 
 /**
- * \brief Internal representation of the whole B-tree.
+ * @brief Internal representation of the whole B-tree.
  */
 typedef struct {
     int order; /**< The order of the tree, i.e. the minimum number of records a non-root node can have. */
@@ -67,15 +71,15 @@ typedef struct {
 } jnx_B_tree;
 
 
-/**
- * \brief Create and initialise a new B-tree. This function must be called first, and once
+/** @fn jnx_B_tree* jnx_B_tree_init(int order, compare_keys callback)
+ * @brief Create and initialise a new B-tree. This function must be called first, and once
  * per tree, before any other tree operations are called.
  *
  * Initialise B-tree with order and callback function which compares the keys of two records.
  *
- * @parameter order The order of the tree, i.e. the minumum number of records held in a 
+ * @param order The order of the tree, i.e. the minumum number of records held in a 
  *                  non-root node.
- * @parameter callback The function that takes to keys as void pointers, and returns an
+ * @param callback The function that takes to keys as void pointers, and returns an
  *                     integer which represents whether the first key is "before", "equal"
  *                     or "after" the second key according to some key ordering. The function
  *                     returns an integer less than 0 if first key is before second key,
@@ -87,62 +91,62 @@ typedef struct {
 jnx_B_tree* jnx_B_tree_init(int order, compare_keys callback);
 
 
-/**
- * \brief Add a record (key, value) to the B-tree.
+/** @fn void jnx_B_tree_add(jnx_B_tree *tree, void *key, void *value)
+ * @brief Add a record (key, value) to the B-tree.
  *
  * This function takes a key and a value for the record, creates the appropriate record
  * and inserts it into the tree. If a record with the same key already exists in the tree
  * the function will just replace the value for that record.
  *
- * @parameter tree The B-tree to which new record is added
- * @parameter key The key for the record
- * @parameter value The value for the record
+ * @param tree The B-tree to which new record is added
+ * @param key The key for the record
+ * @param value The value for the record
  *
- * WARNING: The key and value are not copied they are just used as pointer assignments,
+ * @warning The key and value are not copied they are just used as pointer assignments,
  * so it's important to think about the lifecycle of keys and values you pass to the function.
  */
 void jnx_B_tree_add(jnx_B_tree *tree, void *key, void *value);
 
 
-/** 
- * \brief Retreive the value for key from the B-tree.
+/** @fn void jnx_B_tree_lookup(jnx_B_tree *tree, void *key)
+ * @brief Retreive the value for key from the B-tree.
  *
  * The function returns the pointer to the value, or NULL if the key is not in the B-tree.
  *
- * @parameter tree The B-tree in which to look up the value for key.
- * @parameter key The key for lookup.
+ * @param tree The B-tree in which to look up the value for key.
+ * @param key The key for lookup.
  * 
  * @return Pointer to the value, or NULL if key is not in the tree.
  *
- * WARNING: The value is just a pointer to, not a copy of the actual value so it is very important
+ * @warning The value is just a pointer to, not a copy of the actual value so it is very important
  * to think carefully before you free its memory as it'll be freed from the B-tree record also.
  */
 void *jnx_B_tree_lookup(jnx_B_tree *tree, void *key);
 
 
-/**
- * \brief Remove from the B-tree the record which matches the passed in key.
+/** @fn void jnx_B_tree_remove(jnx_B_tree *tree, void *key)
+ * @brief Remove from the B-tree the record which matches the passed in key.
  *
- * If the record with key "key" is in the tree, the record is removed and
- * the tree is rebalanced if necessary. If the record with key "key"
- * is not in the tree, the tree is unchanged.
+ * If the record with @a key is in the @a tree, the record is removed and
+ * the tree is rebalanced if necessary. If the record with @a key
+ * is not in the @a tree, the tree is unchanged.
  *
- * @parameter tree The B-tree from which to remove the record.
- * @parameter key The key for which to remove the record.
+ * @param tree The B-tree from which to remove the record.
+ * @param key The key for which to remove the record.
  *
- * WARNING: This operation will clean up the internal structures but will not attempt to free
+ * @warning This operation will clean up the internal structures but will not attempt to free
  * the memory for the key and value of the deleted record. It's worth bearing this in mind to
  * avoid memory leaks.
  */
 void jnx_B_tree_remove(jnx_B_tree *tree, void *key);
 
 
-/**
- * \brief Delete the B-tree.
+/** @fn void jnx_B_tree_delete(jnx_B_tree *tree)
+ * @brief Delete the B-tree.
  *
  * Delete all the nodes and records of the B-tree. Upon function return the tree will be empty.
  *
- * @parameter
+ * @param tree The B-tree to delete.
  */
 void jnx_B_tree_delete(jnx_B_tree* tree);
 
