0cf80934740ec535864487186e9c8f81432c2644
alexsimonjones@gmail.com <alexsimonjones@gmail.com>
2013-03-24 15:40:38 +0000

    updated

--- test/test_jnxlist.c
+++ test/test_jnxlist.c
@@ -24,49 +24,31 @@ struct foo{
     int number;
 }foo;
 
-
-
-void looper()
+void test_list_creation()
 {
-    jnx_list *list = jnx_list_init();
-    int x,y;
-    char alphabet = 'a';
-    for(x = 0; x < 26; ++x)
-    {
-        jnx_list_add(list,alphabet);
-        ++alphabet;
-    }
-    alphabet = 'a';
-    for(y = 0; y < x; ++y)
-    {
-        char output = (char)jnx_list_remove(list);
-        assert(output == alphabet);
-        ++alphabet;
-    }
-
-    jnx_list_delete(list);
-
-}
-void structo()
-{
-    looper();
     printf("- test_list_creation");
     jnx_list *secondlist = jnx_list_init();
+    assert(secondlist != NULL);
+    jnx_term_printf_in_color(JNX_COL_GREEN, " OK\n");
     struct foo *f = malloc(sizeof(foo));
     f->number = 10;
- 
+    printf("- test_list_insertion"); 
     jnx_list_add(secondlist,(void*)f); 
- 
+    jnx_term_printf_in_color(JNX_COL_GREEN, " OK\n");
+    printf("- test_list_removal");
     struct foo *output = (struct foo*)jnx_list_remove(secondlist);
     assert(output->number == 10);
+    jnx_term_printf_in_color(JNX_COL_GREEN, " OK\n");
+    printf("- test_list_deletion"); 
     jnx_list_delete(secondlist);
+    jnx_term_printf_in_color(JNX_COL_GREEN, " OK\n");
+    printf("- test_list_deletion"); 
     jnx_term_printf_in_color(JNX_COL_GREEN, " OK\n");
 }
 int main(int args, char **argv)
 {
     printf("Running list tests...\n");
-
-    structo();
+    test_list_creation();
     printf("List tests completed.\n");
     return 0;
 }
