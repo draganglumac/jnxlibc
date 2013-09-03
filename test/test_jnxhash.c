b6ed95bdf7481001e527bc9ab5eb5c77c0ac44be
AlexsJones <jonesax@hush.com>
2013-09-03 13:51:08 +0100

    updating tests

--- test/test_jnxhash.c
+++ test/test_jnxhash.c
@@ -25,25 +25,22 @@ int main(int args, char **argv)
     printf("Running test for jnxhash\n");
     
     //setup
-    printf("- test_hash_insertion");
-    jnx_hashmap *testhash = jnx_hash_init(1024); 
-     
-    jnx_hash_put(testhash,"Alex","Jones");
+	printf("- test_hash_creation");
+	jnx_hashmap *testhash = jnx_hash_init(1024); 
+	assert(testhash);
+	jnx_term_printf_in_color(JNX_COL_GREEN, "  OK\n");
+    printf("- test_hash_insertion");    
+	jnx_hash_put(testhash,"Alex","Jones");
     jnx_hash_put(testhash,"Bob","Marley");
     assert((char*)jnx_hash_get(testhash,"Alex") == "Jones"); 
-
-    jnx_term_printf_in_color(JNX_COL_GREEN, "  OK\n");
-	
+	jnx_term_printf_in_color(JNX_COL_GREEN, "  OK\n");
 	printf("- test_hash_get_keys");
-
 	const char **buffer;
 	int number_of_keys = jnx_hash_get_keys(testhash,&buffer);
 	assert(number_of_keys == 2);
+	jnx_term_printf_in_color(JNX_COL_GREEN, "  OK\n");
 	free(buffer);
-
-    jnx_hash_delete(testhash);  
-    jnx_term_printf_in_color(JNX_COL_GREEN, "  OK\n");
-
+	jnx_hash_delete(testhash);  
 	printf("Hash tests completed.\n"); 
     return 0;
 }
