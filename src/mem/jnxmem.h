773076c926bdb3a350a22fde62f551444c9eb753
Alex Jones <jonesax@hush.com>
2014-01-16 09:23:43 +0000

    Update jnxmem.h

--- src/mem/jnxmem.h
+++ src/mem/jnxmem.h
@@ -11,7 +11,7 @@
 #include <time.h>
 #include <stdio.h>
 #include <string.h>
-#ifdef __cspluplus
+#ifdef __cplusplus
 	extern "C" {
 #endif
 typedef enum { FREE, ALLOC }jnx_mem_memtrace_state;
@@ -95,7 +95,7 @@ void jnx_mem_free(void *ptr);
 #define JNX_MEM_FREE(X)\
 	jnx_mem_free(X);
 
-#ifdef __cspluplus
+#ifdef __cplusplus
 	}
 #endif
 #endif
