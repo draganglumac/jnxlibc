8c3af5831b807e989630ac74eaf33b6af5715acf
AlexsJones <jonesax@hush.comm>
2014-02-20 23:32:14 +0000

    initial network interface documentation

--- src/net/jnxnetwork.h
+++ src/net/jnxnetwork.h
@@ -42,16 +42,56 @@ typedef struct
 typedef void (*listen_callback)(char *, size_t ,char *);
 typedef void (*broadcast_listen_callback)(char *, size_t);
 
+/**
+ * @fn jnx_socket *jnx_network_socket_create(unsigned int addrfamily,ssize_t stype)
+ * @brief creates a jnx socket
+ * @param addrfamily this is the address family to use (e.g. AF_UNIX)
+ * @param stypes this is the socket type to use (e.g. SOCK_STREAM)
+ * @return jnx_socket
+ */
 jnx_socket *jnx_network_socket_create(unsigned int addrfamily,ssize_t stype);
-
+/**
+ * @fn void jnx_network_socket_destroy(jnx_socket *s)
+ * @param s is the socket to destroy
+ */
 void jnx_network_socket_destroy(jnx_socket *s);
-
+/**
+ * @fn jnx_network_send(jnx_socket *s, char *host, ssize_t port, char *msg, ssize_t msg_len)
+ * @param s is the socket to use to send
+ * @param host is the target destination
+ * @param port is the target port
+ * @param msg is the payload to send
+ * @param msg_len is the size of payload
+ * @return size_t of bytes sent, -1 for failure
+ */
 size_t jnx_network_send(jnx_socket *s, char *host, ssize_t port, char *msg, ssize_t msg_len);
-
+/**
+ * @fn size_t jnx_network_listen(jnx_socket *s, ssize_t port, ssize_t max_connections, listen_callback c)
+ * @param s is the socket to use to send
+ * @param port is the target port
+ * @param max_connections are the number of connetions in the queue
+ * @param c is the function pointer callback for received messages
+ * @return -1 on error
+ */
 size_t jnx_network_listen(jnx_socket *s, ssize_t port, ssize_t max_connections, listen_callback c);
-
+/**
+ * @fn size_t jnx_network_broadcast(jnx_socket *S, ssize_t port, char *group, char *msg, ssize_t msg_len)
+ * @param s is the socket to use to send
+ * @param port is the target port
+ * @param group is the broadcast group (e.g. 255.0.0.25)
+ * @param msg is the payload to send
+ * @param msg_len is the size of payload
+ * @return -1 on error 
+ */
 size_t jnx_network_broadcast(jnx_socket *s, ssize_t port, char *group, char *msg,ssize_t msg_len);
-
+/**
+ * @param size_t jnx_network_broadcast_listen(jnx_socket *s, ssize_t port, char *group, broadcast_callback c)
+ * @param s is the socket to use to send
+ * @param port is the target port
+ * @param group is the broadcast group (e.g. 255.0.0.25)
+ * @param c is the function pointer callback for received messages
+ * @return -1 on error
+ */
 size_t jnx_network_broadcast_listen(jnx_socket *s, ssize_t port, char *group, broadcast_listen_callback c);
 
 #ifdef __cplusplus
