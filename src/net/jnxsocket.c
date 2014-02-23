cd9ce40224cfa169d695ef8ee0ded1318dc232fa
AlexsJones <jonesax@hush.comm>
2014-02-23 10:56:53 +0000

    updated

---
+++ src/net/jnxsocket.c
@@ -0,0 +1,276 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  jnxsocket.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/21/14 18:07:02
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  jonesax (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <arpa/inet.h>
+#include <assert.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <net/if.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include "jnxlog.h"
+#include "jnxsocket.h"
+#define MAXBUFFER 1024
+jnx_socket *create_socket(unsigned int type,unsigned int addrfamily)
+{
+	assert(type);
+	assert(addrfamily);
+	int sock = socket(addrfamily,type,0);
+	if(sock < 0)
+	{
+		return NULL;
+	}
+	int optval = 1;
+	setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&optval,sizeof optval);
+	jnx_socket *s = malloc(sizeof(jnx_socket));
+	s->isclosed = 0;
+	s->addrfamily = addrfamily;
+	s->socket = sock;
+	s->stype = type;
+	return s;
+}
+jnx_socket *jnx_socket_tcp_create(unsigned int addrfamily)
+{
+	return create_socket(SOCK_STREAM,addrfamily);
+}
+jnx_socket *jnx_socket_udp_create(unsigned int addrfamily)
+{
+	return create_socket(SOCK_DGRAM,addrfamily);
+}
+void jnx_socket_close(jnx_socket *s)
+{
+	if(!s->isclosed)
+	{
+		if(s->socket != -1)
+		{
+			close(s->socket);
+			s->isclosed = 1;
+		}
+	}
+}
+void jnx_socket_destroy(jnx_socket **s)
+{
+	jnx_socket_close(*s);
+	free(*s);
+	*s = NULL;	
+}
+size_t jnx_socket_tcp_send(jnx_socket *s, char *host, char* port, char *msg, ssize_t msg_len)
+{
+	assert(s);
+	assert(host);
+	assert(port);
+	assert(msg);
+	assert(msg_len);
+	assert(s->isclosed == 0);
+	assert(s->stype == SOCK_STREAM);
+	struct addrinfo hints, *res;
+	memset(&hints,0,sizeof(hints));
+	hints.ai_family = s->addrfamily;
+	hints.ai_socktype = s->stype;
+
+	getaddrinfo(host,port,&hints,&res);
+
+	if(connect(s->socket,res->ai_addr,res->ai_addrlen) != 0)
+	{
+		perror("connect:");
+		freeaddrinfo(res);
+		return -1;
+	}
+	freeaddrinfo(res);
+
+	size_t tbytes = 0;
+	size_t rbytes = msg_len;
+
+	while(tbytes < rbytes)
+	{
+		size_t n = write(s->socket,msg,rbytes);
+		if(n == -1)
+		{
+			perror("send:");
+			return -1;
+		}
+		tbytes +=n;
+		rbytes = msg_len - tbytes;
+		JNX_LOGC("Sent [%zu/%zubytes]\n",tbytes,msg_len);
+	}	
+	return tbytes;
+}
+size_t jnx_socket_udp_send(jnx_socket *s, char *host, char* port, char *msg, ssize_t msg_len)
+{
+	assert(s);
+	assert(host);
+	assert(port);
+	assert(msg);
+	assert(msg_len);
+	assert(s->isclosed == 0);
+	assert(s->stype == SOCK_DGRAM);
+	struct addrinfo hints, *res;
+	memset(&hints,0,sizeof(hints));
+	hints.ai_family = s->addrfamily;
+	hints.ai_socktype = s->stype;
+	hints.ai_flags =0;
+	hints.ai_protocol = IPPROTO_UDP;
+	hints.ai_next = NULL;	
+	getaddrinfo(host,port,&hints,&res);
+
+	size_t tbytes = 0;
+	size_t rbytes = msg_len;
+
+	while(tbytes < rbytes)
+	{
+		size_t n = sendto(s->socket,msg,msg_len,0,res->ai_addr,res->ai_addrlen);
+		if(n == -1)
+		{
+			perror("send:");
+			return -1;
+		}
+		tbytes +=n;
+		rbytes = msg_len - tbytes;
+		JNX_LOGC("Sent [%zu/%zubytes]\n",tbytes,msg_len);
+	}	
+	return tbytes;
+}
+size_t jnx_socket_tcp_listen(jnx_socket *s, char* port, ssize_t max_connections, listen_callback c)
+{
+	assert(s);
+	assert(port);
+	assert(s->isclosed == 0);
+	assert(s->stype == SOCK_STREAM);
+	int optval = 1;
+	struct addrinfo hints, *res, *p;
+	char buffer[MAXBUFFER];
+	memset(&hints,0,sizeof(hints));
+	hints.ai_family = s->addrfamily;
+	hints.ai_socktype = s->stype;
+	getaddrinfo(NULL,port,&hints,&res);
+	for(p=res;p != NULL;p->ai_next)
+	{
+		if((s->socket = socket(p->ai_family,p->ai_socktype,p->ai_protocol)) == -1)
+		{
+			perror("server: socket");
+			continue;
+		}
+		if (setsockopt(s->socket, SOL_SOCKET, SO_REUSEADDR, &optval,
+					sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+		if (bind(s->socket, p->ai_addr, p->ai_addrlen) == -1) {
+			perror("server: bind");
+			continue;
+		}
+		break;
+	}
+	freeaddrinfo(res);
+	listen(s->socket,max_connections);
+	while(1)
+	{
+		struct sockaddr_storage their_addr;
+		socklen_t addr_size = sizeof(their_addr);
+		int recfd = accept(s->socket,(struct sockaddr*)&their_addr,&addr_size);
+		if(recfd < 0)
+		{
+			JNX_LOGC("accept: %s",strerror(errno));
+			return -1;
+		}
+		bzero(buffer,MAXBUFFER);
+		FILE *fp = tmpfile();
+		size_t bytesread = read(recfd,buffer,MAXBUFFER);
+		fwrite(buffer,sizeof(char),bytesread,fp);
+
+		while(bytesread > 0)
+		{
+			bzero(buffer,MAXBUFFER);
+			bytesread = read(recfd,buffer,MAXBUFFER);
+			if(bytesread == -1)
+			{
+				perror("read:");
+				fclose(fp);
+				return -1;
+			}
+			if(bytesread > 0)
+			{
+				fwrite(buffer,sizeof(char),bytesread,fp);
+			}
+		}
+		int len = ftell(fp);
+		rewind(fp);
+		char *out = calloc(len + 1, sizeof(char));
+		fread(out,1,len,fp);
+		fclose(fp);
+		c(out,len,s);
+	}
+	return 0;
+}
+size_t jnx_socket_udp_listen(jnx_socket *s, char* port, ssize_t max_connections, listen_callback c)
+{
+	assert(s);
+	assert(port);
+	assert(s->isclosed == 0);
+	assert(s->stype == SOCK_DGRAM);
+	int optval = 1;
+	struct addrinfo hints, *res, *p;
+	struct sockaddr_storage their_addr;
+	socklen_t addr_len;
+	char buffer[MAXBUFFER];
+	memset(&hints,0,sizeof(hints));
+	hints.ai_family = s->addrfamily;
+	hints.ai_socktype = s->stype;
+	hints.ai_flags = AI_PASSIVE;
+	getaddrinfo(NULL,port,&hints,&res);
+	for(p=res;p != NULL;p->ai_next)
+	{
+		if((s->socket = socket(p->ai_family,p->ai_socktype,p->ai_protocol)) == -1)
+		{
+			perror("server: socket");
+			continue;
+		}
+		if (setsockopt(s->socket, SOL_SOCKET, SO_REUSEADDR, &optval,
+					sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+		if (bind(s->socket, p->ai_addr, p->ai_addrlen) == -1) {
+			perror("server: bind");
+			continue;
+		}
+		break;
+	}
+	freeaddrinfo(res);
+
+	while(1){
+		addr_len = sizeof(their_addr);
+		bzero(buffer,MAXBUFFER);
+		size_t tbytes = recvfrom(s->socket,buffer,MAXBUFFER,0,(struct sockaddr *)&their_addr,
+				&addr_len);
+		if(tbytes == -1)
+		{
+			perror("recvfrom:");
+			return -1;
+		}
+		c(buffer,tbytes,s);
+	}
+	return -1;
+}
---
+++ src/net/jnxsocket.h
@@ -0,0 +1,113 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  jnxsocket.h
+ *
+ *    Description:  Low level network API 
+ *
+ *        Version:  1.0
+ *        Created:  02/20/14 12:50:15
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  jonesax (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+/** @file jnxsocket.h
+ *  @brief The API for jnxlibc networking (includes TCP, UDP )
+ *
+ *  #include <jnxc_headers/jnxsocket.h>
+ */
+#ifndef __JNXNETWORK_H__
+#define __JNXNETWORK_H__
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#ifdef __cplusplus
+	extern "C" {
+#endif
+typedef struct 
+{
+	int isclosed;
+	int socket;
+	unsigned int addrfamily;
+	ssize_t stype;
+} jnx_socket;
+
+typedef void (*listen_callback)(char *, size_t ,jnx_socket *);
+typedef void (*broadcast_listen_callback)(char *, size_t,jnx_socket *);
+
+/**
+ * @fn jnx_socket *jnx_socket_tcp_socket_create(unsigned int addrfamily,ssize_t stype)
+ * @brief creates a jnx socket
+ * @param addrfamily this is the address family to use (e.g. AF_UNIX)
+ * @param stypes this is the socket type to use (e.g. SOCK_STREAM)
+ * @return jnx_socket
+ */
+jnx_socket *jnx_socket_tcp_create(unsigned int addrfamily);
+/**
+ * @fn jnx_socket *jnx_socket_udp_socket_create(unsigned int addrfamily,ssize_t stype)
+ * @brief creates a jnx socket
+ * @param addrfamily this is the address family to use (e.g. AF_UNIX)
+ * @param stypes this is the socket type to use (e.g. SOCK_STREAM)
+ * @return jnx_socket
+ */
+jnx_socket *jnx_socket_udp_create(unsigned int addrfamily);
+/** 
+ *@fn jnx_socket_socket_close(jnx_socket *s)
+ *@brief close the socket but maintain the socket object
+ *@params s the socket object to close
+ */
+void jnx_socket_close(jnx_socket *s);
+/**
+ * @fn void jnx_socket_socket_destroy(jnx_socket *s)
+ * @param s is the socket to destroy
+ */
+void jnx_socket_destroy(jnx_socket **s);
+/**
+ * @fn jnx_socket_tcp_send(jnx_socket *s, char *host, ssize_t port, char *msg, ssize_t msg_len)
+ * @param s is the socket to use to send
+ * @param host is the target destination
+ * @param port is the target port
+ * @param msg is the payload to send
+ * @param msg_len is the size of payload
+ * @return size_t of bytes sent, -1 for failure
+ */
+size_t jnx_socket_tcp_send(jnx_socket *s, char *host, char* port, char *msg, ssize_t msg_len);
+/**
+ * @fn jnx_socket_udp_send(jnx_socket *s, char *host, ssize_t port, char *msg, ssize_t msg_len)
+ * @param s is the socket to use to send
+ * @param host is the target destination
+ * @param port is the target port
+ * @param msg is the payload to send
+ * @param msg_len is the size of payload
+ * @return size_t of bytes sent, -1 for failure
+ */
+size_t jnx_socket_udp_send(jnx_socket *s, char *host, char* port, char *msg, ssize_t msg_len);
+/**
+ * @fn size_t jnx_socket_tpc_listen(jnx_socket *s, ssize_t port, ssize_t max_connections, listen_callback c)
+ * @param s is the socket to use to send
+ * @param port is the target port
+ * @param max_connections are the number of connetions in the queue
+ * @param c is the function pointer callback for received messages
+ * @return -1 on error
+ */
+size_t jnx_socket_tcp_listen(jnx_socket *s, char* port, ssize_t max_connections, listen_callback c);
+/**
+ * @fn size_t jnx_socket_udp_listen(jnx_socket *s, ssize_t port, ssize_t max_connections, listen_callback c)
+ * @param s is the socket to use to send
+ * @param port is the target port
+ * @param max_connections are the number of connetions in the queue
+ * @param c is the function pointer callback for received messages
+ * @return -1 on error
+ */
+size_t jnx_socket_udp_listen(jnx_socket *s, char* port, ssize_t max_connections, listen_callback c);
+#ifdef __cplusplus
+	}
+#endif
+#endif
