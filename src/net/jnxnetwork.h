105dcc934f10acdfb25f19fdfd99db9dc8df9721
AlexsJones <jonesax@hush.comm>
2014-03-02 17:58:57 +0000

    added ip from interface

---
+++ src/net/jnxnetwork.h
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  jnxnetwork.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/02/14 17:09:37
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  jonesax (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef __JNXNETWORK_H__
+#define __JNXNETWORK_H__
+#ifdef __cplusplus
+	extern "C" {
+#endif
+char *jnx_network_get_ip(char *interface,unsigned int addrfamily);
+#ifdef __cplusplus
+	}
+#endif
+#endif
