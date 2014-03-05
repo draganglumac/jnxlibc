a1d23224078cbe344ef4efff61ba778b1703e745
AlexsJones <jonesax@hush.comm>
2014-03-05 08:03:39 +0000

    formatting update

--- src/bug/jnxdebug.c
+++ src/bug/jnxdebug.c
@@ -3,15 +3,15 @@
  *
  *       Filename:  jnxdebug.c
  *
- *    Description:  
+ *    Description:
  *
  *        Version:  1.0
  *        Created:  11/27/13 18:59:18
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
- *   Organization:  
+ *         Author:  YOUR NAME (),
+ *   Organization:
  *
  * =====================================================================================
  */
@@ -26,32 +26,28 @@
 #include <string.h>
 typedef void (*jnx_debug_internal_handler)(int);
 static char *write_path = NULL;
-void jnx_debug_stacktrace_cb(int s)
-{
-	void *array[MAX_TRACE];
-	size_t size;
-	char **str;
-	size_t i;
-	size = backtrace(array,MAX_TRACE);
-	str=backtrace_symbols(array,size);
-	for(i = 0; i < size; ++i)
-	{
-		printf("[%zu]%s\n",i,str[i]);
-		int len = strlen(str[i]);
-		str[i][len] = '\n';
-		jnx_file_write(write_path,str[i],strlen(str[i]),"a");
-	}
-	free(str);
-	exit(1);
+void jnx_debug_stacktrace_cb(int s) {
+    void *array[MAX_TRACE];
+    size_t size;
+    char **str;
+    size_t i;
+    size = backtrace(array,MAX_TRACE);
+    str=backtrace_symbols(array,size);
+    for(i = 0; i < size; ++i) {
+        printf("[%zu]%s\n",i,str[i]);
+        int len = strlen(str[i]);
+        str[i][len] = '\n';
+        jnx_file_write(write_path,str[i],strlen(str[i]),"a");
+    }
+    free(str);
+    exit(1);
 }
-void jnx_debug_stacktrace(char *path, int counter,int signals[])
-{
-	write_path = path;
-	assert(write_path);	
-	jnx_debug_internal_handler c = jnx_debug_stacktrace_cb;
-	int i;
-	for(i=0;i<counter;++i)
-	{
-		signal(signals[i],c);
-	}
+void jnx_debug_stacktrace(char *path, int counter,int signals[]) {
+    write_path = path;
+    assert(write_path);
+    jnx_debug_internal_handler c = jnx_debug_stacktrace_cb;
+    int i;
+    for(i=0; i<counter; ++i) {
+        signal(signals[i],c);
+    }
 }
--- src/net/jnxnetwork.c
+++ src/net/jnxnetwork.c
@@ -3,15 +3,15 @@
  *
  *       Filename:  jnxnetwork.c
  *
- *    Description:  
+ *    Description:
  *
  *        Version:  1.0
  *        Created:  03/02/14 17:11:09
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  jonesax (), 
- *   Organization:  
+ *         Author:  jonesax (),
+ *   Organization:
  *
  * =====================================================================================
  */
@@ -31,46 +31,40 @@
 #include <linux/if_link.h>
 #include <stdlib.h>
 #include "jnxnetwork.h"
-char *jnx_network_get_ip(char *interface,unsigned int addrfamily)
-{
-	struct ifaddrs *ifaddr, *ifa;
-	char host[NI_MAXHOST];
-	bzero(host,NI_MAXHOST);
-	int n,s,family;
-	if(getifaddrs(&ifaddr) != 0)
-	{
-		perror("getifaddrs:");
-		return NULL;
-	}
-	for(ifa = ifaddr, n =0; ifa != NULL; ifa=ifa->ifa_next, ++n)
-	{
-		if(ifa->ifa_addr == NULL)
-			continue;
-		
-		if(strcmp(ifa->ifa_name,interface) == 0)
-		{
-			family = ifa->ifa_addr->sa_family;
-			if(family == addrfamily)
-			{	
-				s = getnameinfo(ifa->ifa_addr,
-						(family == AF_INET) ? 
-						sizeof(struct sockaddr_in) : 
-						sizeof(struct sockaddr_in6), 
-						host, 
-						NI_MAXHOST,
-						NULL,
-						0,
-						NI_NUMERICHOST | NI_NUMERICSERV);
-				if(s != 0)
-				{
-					perror("getnameinfo:");
-					return NULL;
-				}
-				freeifaddrs(ifaddr);
-				return strndup(host,strlen(host));
-			}
-		}
-	}
-	freeifaddrs(ifaddr);
-	return NULL;
+char *jnx_network_get_ip(char *interface,unsigned int addrfamily) {
+    struct ifaddrs *ifaddr, *ifa;
+    char host[NI_MAXHOST];
+    bzero(host,NI_MAXHOST);
+    int n,s,family;
+    if(getifaddrs(&ifaddr) != 0) {
+        perror("getifaddrs:");
+        return NULL;
+    }
+    for(ifa = ifaddr, n =0; ifa != NULL; ifa=ifa->ifa_next, ++n) {
+        if(ifa->ifa_addr == NULL)
+            continue;
+
+        if(strcmp(ifa->ifa_name,interface) == 0) {
+            family = ifa->ifa_addr->sa_family;
+            if(family == addrfamily) {
+                s = getnameinfo(ifa->ifa_addr,
+                                (family == AF_INET) ?
+                                sizeof(struct sockaddr_in) :
+                                sizeof(struct sockaddr_in6),
+                                host,
+                                NI_MAXHOST,
+                                NULL,
+                                0,
+                                NI_NUMERICHOST | NI_NUMERICSERV);
+                if(s != 0) {
+                    perror("getnameinfo:");
+                    return NULL;
+                }
+                freeifaddrs(ifaddr);
+                return strndup(host,strlen(host));
+            }
+        }
+    }
+    freeifaddrs(ifaddr);
+    return NULL;
 }
