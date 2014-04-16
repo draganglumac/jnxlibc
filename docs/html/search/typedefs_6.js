34cfcc6e38e67644dcb764e48349b471bd14d1db
Dragan Glumac <dragan.glumac@gmail.com>
2014-04-16 06:28:37 +0100

    Merged Alex's master changes and generated new HTML docs and man pages.

--- docs/html/search/typedefs_6.js
+++ docs/html/search/typedefs_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['udp_5fsocket_5flistener_5fcallback',['udp_socket_listener_callback',['../jnxsocket_8h.html#ae422f8adfa5d532123f3feb8cbcee841',1,'jnxsocket.h']]]
+  ['udp_5fsocket_5flistener_5fcallback',['udp_socket_listener_callback',['../jnxsocket_8h.html#a6d94421ace777eeda79cf7d5dd5515fe',1,'jnxsocket.h']]]
 ];
