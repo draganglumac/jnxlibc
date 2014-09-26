2cbf6f532d597d4075ded2819abbf152db2431d7
Dragan Glumac <dragan.glumac2@bskyb.com>
2014-09-26 13:36:04 +0100

    Changed the implementations of list traversal.

--- src/dat/jnxlist.c
+++ src/dat/jnxlist.c
@@ -21,166 +21,150 @@
 #include "jnxcheck.h"
 #include <stdio.h>
 
+static jnx_node *make_new_node(void *_datain) {
+	jnx_node *node = malloc(sizeof(jnx_node));
+	node->_data = _datain;
+	node->next_node = NULL;
+	node->prev_node = NULL;
+	return node;
+}
 jnx_list* jnx_list_create(void) {
-  jnx_list *list = malloc(sizeof(jnx_list));
-  list->head = NULL;
-  list->tail = NULL;
-  list->counter = 0;
-  list->internal_lock = jnx_thread_mutex_create();
-  return list;
+	jnx_list *list = malloc(sizeof(jnx_list));
+	list->head = NULL;
+	list->tail = NULL;
+	list->counter = 0;
+	list->internal_lock = jnx_thread_mutex_create();
+	return list;
 }
 void jnx_list_add(jnx_list* A, void* _datain) {
-  JNXCHECK(A);
-  JNXCHECK(_datain);
-  if(A->head == NULL) {
-    jnx_node *node = malloc(sizeof(jnx_node));
-    node->_data = _datain;
-    node->next_node = NULL;
-    node->prev_node = NULL;
-    A->head = node;
-    A->tail = node;
-    A->counter++;
-    return;
-  }
-  jnx_node *marker = A->head;
-  while(A->head) {
-    jnx_node *current_node = A->head;
-    if(!A->head->next_node) {
-      jnx_node *node = malloc(sizeof(jnx_node));
-      node->_data = _datain;
-      node->next_node = NULL;
-      node->prev_node = current_node;
-      current_node->next_node = node;
-      A->counter++;
-      A->tail = node;
-      A->head = marker;
-      return;
-    }
-    A->head = A->head->next_node;
-  }
-  A->head = marker;
+	JNXCHECK(A);
+	JNXCHECK(_datain);
+	jnx_node *node = make_new_node(_datain);
+	jnx_node *current = A->head;
+	if (current == NULL) {
+		A->head = node;
+	}
+	else {
+		while (current->next_node != NULL)
+			current = current->next_node;
+		current->next_node = node;
+		node->prev_node = current;
+	}
+	A->tail = node;
+	A->counter++;
+	return;
 }
 void jnx_list_add_ts(jnx_list* A, void* _datain) {
-  jnx_thread_lock(A->internal_lock);
-  jnx_list_add(A,_datain);
-  jnx_thread_unlock(A->internal_lock);
+	jnx_thread_lock(A->internal_lock);
+	jnx_list_add(A,_datain);
+	jnx_thread_unlock(A->internal_lock);
 }
 void* jnx_list_remove(jnx_list** A) {
-  if((*A)->head == NULL) {
-    return NULL;
-  }
-  if((*A)->head->next_node == NULL) {
-    void *data = (*A)->head->_data;
-    free((*A)->head);
-    (*A)->head = NULL;
-    (*A)->counter--;
-    return data;
-  }
-  jnx_node *origin_head = (*A)->head;
-  while((*A)->head) {
-    jnx_node *next = (*A)->head->next_node;
-    if(next->next_node == NULL) {
-      void *data = next->_data;
-      free(next);
-      (*A)->head->next_node = NULL;
-      (*A)->head = origin_head;
-      (*A)->counter--;
-      return data;
-    }
-    (*A)->head = next;
-  }
-  (*A)->head = origin_head;
-  return NULL;
+	if((*A)->head == NULL) {
+		return NULL;
+	}
+	jnx_node *last = (*A)->tail;
+	jnx_node *penultimate = last->prev_node;
+	(*A)->tail = penultimate;
+	if (penultimate == NULL) {
+		(*A)->head = NULL;
+	}
+	else {
+		penultimate->next_node = NULL;
+	}
+	void *data = last->_data;
+	free(last);
+	(*A)->counter--;
+	return data;
 }
 void* jnx_list_remove_ts(jnx_list** A) {
-  jnx_list *l = *A;
-  jnx_thread_lock(l->internal_lock);
-  void *ret = jnx_list_remove(A);
-  jnx_thread_unlock(l->internal_lock);
-  return ret;
+	jnx_list *l = *A;
+	jnx_thread_lock(l->internal_lock);
+	void *ret = jnx_list_remove(A);
+	jnx_thread_unlock(l->internal_lock);
+	return ret;
 }
 
 void *jnx_list_remove_front(jnx_list ** A) {
-  if((*A)->head == NULL) {
-    return NULL;
-  }
-  if((*A)->head->next_node == NULL) {
-    void *data = (*A)->head->_data; 
-    free((*A)->head);
-    (*A)->head = NULL;
-    (*A)->counter =0;
-    return data;
-  }
-  void *data = (*A)->head->_data;
-  free((*A)->head);
-  jnx_node *next = (*A)->head->next_node;
-  next->prev_node = NULL;
-  (*A)->head = next;
-  (*A)->counter--;
-  return data;
+	if((*A)->head == NULL) {
+		return NULL;
+	}
+	jnx_node *first = (*A)->head;
+	jnx_node *second = first->next_node;
+	(*A)->head = second;
+	if (second == NULL) {
+		(*A)->tail = NULL;
+	}
+	else {
+		second->prev_node = NULL;
+	}
+	void *data = first->_data; 
+	free(first);
+	(*A)->counter--;
+	return data;
 }
 void *jnx_list_remove_front_ts(jnx_list ** A) {
-  jnx_list *l = *A;
-  jnx_thread_lock(l->internal_lock);
-  void *ret = jnx_list_remove_front(A);
-  jnx_thread_unlock(l->internal_lock);
-  return ret;
+	jnx_list *l = *A;
+	jnx_thread_lock(l->internal_lock);
+	void *ret = jnx_list_remove_front(A);
+	jnx_thread_unlock(l->internal_lock);
+	return ret;
 }
 size_t jnx_list_count(jnx_list *A) {
-  size_t count = A->counter;
-  return count;
+	size_t count = A->counter;
+	return count;
 }
 size_t jnx_list_count_ts(jnx_list *A) {
-  jnx_thread_lock(A->internal_lock);
-  size_t count = jnx_list_count(A);
-  jnx_thread_unlock(A->internal_lock);
-  return count;
+	jnx_thread_lock(A->internal_lock);
+	size_t count = jnx_list_count(A);
+	jnx_thread_unlock(A->internal_lock);
+	return count;
 }
 int jnx_list_contains(jnx_list *A, void *datain, int (*list_comparison)(void *a,void *b) ){
-  JNXCHECK(A);
-  JNXCHECK(datain);
-  JNXCHECK(list_comparison);
-  jnx_node *head = A->head;
-  int f=0; 
-  while(A->head) {
-    if(list_comparison(datain,A->head->_data)) {
-      f = 1;
-    }
-    A->head = A->head->next_node;
-  }
-  A->head = head;
-  return f;
+	JNXCHECK(A);
+	JNXCHECK(datain);
+	JNXCHECK(list_comparison);
+	jnx_node *current = A->head;
+	int f=0; 
+	while(current) {
+		if(list_comparison(datain,current->_data)) {
+			f = 1;
+			break;
+		}
+		current = current->next_node;
+	}
+	return f;
 }
 int jnx_list_contains_ts(jnx_list *A, void *datain, int (*list_comparison)(void *a,void *b) ){
-  jnx_thread_lock(A->internal_lock);
-  int f = jnx_list_contains(A,datain,list_comparison);
-  jnx_thread_unlock(A->internal_lock);
-  return f;
+	jnx_thread_lock(A->internal_lock);
+	int f = jnx_list_contains(A,datain,list_comparison);
+	jnx_thread_unlock(A->internal_lock);
+	return f;
 }
 void jnx_list_destroy(jnx_list** A) {
-  if((*A) == NULL) {
-    JNX_LOG(DEFAULT_CONTEXT,"jnx_list_destroy: No list\n");
-    return;
-  }
-  if((*A)->head == NULL) {
-    (*A) = NULL;
-    return;
-  }
-  jnx_node *current = (*A)->head;
-  if(!current->next_node) {
+	if((*A) == NULL) {
+		JNX_LOG(DEFAULT_CONTEXT,"jnx_list_destroy: No list\n");
+		return;
+	}
+	if((*A)->head == NULL) {
+		(*A) = NULL;
+		return;
+	}
+	jnx_node *current = (*A)->head;
+	if(!current->next_node) {
 
-    free(current);
-    free(*A);
-    (*A) = NULL;
-    return;
-  }
-  while(current) {
-    jnx_node *current_node = current;
-    current = current->next_node;
-    free(current_node);
-  }
-  jnx_thread_mutex_destroy(&(*A)->internal_lock);
-  free(*A);
-  (*A) = NULL;
+		free(current);
+		free(*A);
+		(*A) = NULL;
+		return;
+	}
+	while(current) {
+		jnx_node *current_node = current;
+		current = current->next_node;
+		free(current_node);
+	}
+	jnx_thread_mutex_destroy(&(*A)->internal_lock);
+	free(*A);
+	(*A) = NULL;
 }
 
