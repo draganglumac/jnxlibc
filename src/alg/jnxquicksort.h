b268ca33b8dd094971982f13b110f5bdc7f96622
Dragan Glumac <dragan.glumac2@bskyb.com>
2014-10-28 15:26:50 +0000

    Added doxygen comments for jnx_quicksort.

--- src/alg/jnxquicksort.h
+++ src/alg/jnxquicksort.h
@@ -1,26 +1,26 @@
-/*
- * =====================================================================================
- *
- *       Filename:  quicksort.h
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  23/10/2014 14:28:35
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Dragan Glumac (), 
- *   Organization:  
- *
- * =====================================================================================
- */
+/** @file jnxquicksort.h
+ * @brief The API for sorting arrays of void* data items that can be ordered, 
+ * via Quicksort algorithm with median-of-three pivot choice strategy.
+*/
 
 #ifndef __JNXQUICKSORT_H__
 #define __JNXQUICKSORT_H__
 
 typedef int(*compare_function)(void *, void *);
 
+/*
+ * @fn jnx_quicksort(void **array, int size, compare_function cf);
+ * @brief Sorts the array based on the comparison function cf.
+ * @param array - the array to sort, it should store data as void * pointers
+ * or be cast into that type if the data structure allows it.
+ * @param size - the size of the array
+ * @param cf - compare function that takes two void *pointers, which will be
+ * elements of the array, and returns < 0 if first is "smaller" than second, 
+ * 0 if they're equal, or > 0 if first is "larger" than second.
+ *
+ * The array is sorted in place and the original array will contain elements
+ * in sorted order according to the order imposed by cf.
+ */
 void jnx_quicksort(void **array, int size, compare_function cf);
 
 #endif // __JNXQUICKSORT_H__
