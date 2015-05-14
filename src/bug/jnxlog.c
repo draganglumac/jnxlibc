403067c99d31a78782dabf25ae73c9ef4ef3cb04
tibbar <tibbar@debbi>
2015-05-14 15:00:45 +0100

    simple logging system

--- src/bug/jnxlog.c
+++ src/bug/jnxlog.c
@@ -1,49 +1,26 @@
-/*
- * =====================================================================================
- *
- *       Filename:  jnxlog.c
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  18/08/2014 18:34:40
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  jonesax (jonesax@hush.com), 
- *   Organization:  
- *
- * =====================================================================================
- */
+/*********************************************************************************
+ *     File Name           :     /home/tibbar/Documents/logger/jnxlog.c
+ *     Created By          :     tibbar
+ *     Creation Date       :     [2015-05-14 14:08]
+ *     Last Modified       :     [2015-05-14 14:56]
+ *     Description         :      
+ **********************************************************************************/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sys/time.h>
+#include <time.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdarg.h>
 #include "jnxlog.h"
-#include "jnxcheck.h"
-#include "jnxfile.h"
 
 #define MAX_SIZE 2048
 #define TIMEBUFFER 256
-jnx_log_config* jnx_log_create(const jnx_char *path,jnx_log_type output){  
-  jnx_log_config *conf = malloc(sizeof(jnx_log_config));
-  conf->log_path = path;
-  conf->output = output;
-  conf->pstart = malloc(sizeof(struct timeval));
-  conf->pend = malloc(sizeof(struct timeval));
-  gettimeofday(conf->pstart,NULL);
-  conf->pcurrent = 0;
-  conf->internal_lock = jnx_thread_mutex_create();
-  return conf;
-}
-void jnx_log(jnx_log_config *config, const jnx_char *file, const jnx_char *function,const jnx_uint32 line,const jnx_char *format,...){
-  JNXCHECK(file);
-  JNXCHECK(function);
-  JNXCHECK(format);
+void jnx_log(jnx_int l, const jnx_char *file, 
+    const jnx_char *function, 
+    const jnx_uint32 line,const jnx_char *format,...) {
   jnx_char buffer[MAX_SIZE];
   jnx_char msgbuffer[MAX_SIZE];
   memset(buffer,0,MAX_SIZE);
@@ -52,38 +29,30 @@ void jnx_log(jnx_log_config *config, const jnx_char *file, const jnx_char *funct
   va_start(ap,format);
   vsprintf(msgbuffer,format,ap);
   va_end(ap);
-  if(config == DEFAULT_CONTEXT) {
-    time_t ptime;
-    time(&ptime);
-    jnx_char pbuffer[TIMEBUFFER];
-    sprintf(pbuffer,"%s",ctime(&ptime));
-    pbuffer[strlen(pbuffer)-1] = '\0';
-    sprintf(buffer,"[%s][%s:%d][t:%s]%s\n",file,function,line,pbuffer, msgbuffer);
-    printf("%s",buffer);
-    return;
-  }
-  sprintf(buffer,"[%s][%s:%d][t:%f]%s\n",file,function,line,config ? config->pcurrent : 0, msgbuffer);
-  switch(config->output) {
-    case FILETYPE:
-      JNXCHECK(config->log_path);
-      jnx_thread_lock(config->internal_lock);
-      jnx_file_write((jnx_char*)config->log_path ? (jnx_char*)config->log_path : "default.log",buffer,strlen(buffer),"a");
-      jnx_thread_unlock(config->internal_lock);
-      break;
-    case CONSOLETYPE:
-      printf("%s",buffer);
-      break;
-  }
-  gettimeofday(config->pend,NULL);
-  jnx_double elapsed_time = ((*config->pend).tv_sec - (*config->pstart).tv_sec) * 1000.0;
-  elapsed_time += ((*config->pend).tv_usec - (*config->pstart).tv_usec) / 1000.0;
-  config->pcurrent = elapsed_time;
-}
-void jnx_log_destroy(jnx_log_config **config){
-  JNXCHECK(*config);
-  jnx_thread_mutex_destroy(&(*config)->internal_lock);
-  free((*config)->pstart);
-  free((*config)->pend);
-  free(*config);
-  *config = NULL;
+  time_t ptime;
+  time(&ptime);
+  jnx_char pbuffer[TIMEBUFFER];
+  sprintf(pbuffer,"%s",ctime(&ptime));
+  pbuffer[strlen(pbuffer)-1] = '\0';
+  sprintf(buffer,"[%s][%s:%d][t:%s]%s\n",file,function,line,pbuffer,msgbuffer);
+
+#ifdef DEBUG
+  printf("%s",buffer);
+#endif
+#ifdef INFO
+  if(l == LDEBUG) return;
+  printf("%s",buffer);
+#endif
+#ifdef WARN
+  if(l == LDEBUG || l == LINFO) return;
+  printf("%s",buffer);
+#endif
+#ifdef ERROR
+  if(l == LDEBUG || l == LINFO || l == LWARN) return;
+  printf("%s",buffer);
+#endif
+#ifdef PANIC
+  if(l == LDEBUG || l == LINFO || l == LWARN || l == LERROR) return;
+  printf("%s",buffer);
+#endif
 }
--- src/bug/jnxlog.h
+++ src/bug/jnxlog.h
@@ -1,94 +1,30 @@
-/*
- * =====================================================================================
- *
- *       Filename:  jnxlog.h
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  18/08/2014 18:23:34
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  YOUR NAME (), 
- *   Organization:  
- *
- * =====================================================================================
- */
+/*********************************************************************************
+ *     File Name           :     jnxlog.h
+ *     Created By          :     tibbar
+ *     Creation Date       :     [2015-05-14 14:01]
+ *     Last Modified       :     [2015-05-14 14:59]
+ *     Description         :      
+ **********************************************************************************/
 #ifndef __JNXLOG_H__
 #define __JNXLOG_H__
-#include <time.h>
 #include "jnxtypes.h"
-#include "jnxthread.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
-#define DEFAULT_CONTEXT NULL
-  typedef enum jnx_log_type {
-    CONSOLETYPE,
-    FILETYPE
-  } jnx_log_type;
 
-  typedef struct jnx_log_config{
-    const jnx_char *log_path;
-    jnx_log_type output;
-    struct timeval *pstart;
-    struct timeval *pend;
-    jnx_double pcurrent;
-    jnx_thread_mutex *internal_lock;
-  } jnx_log_config;
+#define LDEBUG 0
+#define LINFO 1
+#define LWARN 2
+#define LERROR 3
+#define LPANIC 4
 
-  /** @fn jnx_log_create(const jnx_char *path, jnx_log_type output)
-   *  @brief creates a log context for the given logtype 
-   *  @param path is the filepath to set for an output of FILETYPE, can be NULL if using CONSOLETYPE
-   *  @param output defines the logger type, either FILETYPE or CONSOLETYPE
-   *  @return jnx_log_config which is the current log context
-   */
-  jnx_log_config* jnx_log_create(const jnx_char *path,jnx_log_type output);
-  /** @fn jnx_log_destroy(jnx_log_config **config)
-   *  @brief destroys the current log context
-   *  @param config is a pointer to pointer of current log context
-   *  @warning passing a null object will trigger an assertion
-   */
-  void jnx_log_destroy(jnx_log_config **config);
-  /** @fn jnx_log(jnx_log_config *config, const jnx_char *file, const jnx_char *function, const int line, const jnx_char *format,...)
-   *  @brief logs a message to the context or defaults to stdout
-   *  @param conifg this is the context to use for writing, can be NULL and will default to stdout
-   *  @param file current file name [auto generated when using JNX_LOG macro]
-   *  @param function name [auto generated when using JNX_LOG macro]
-   *  @param line number [auto generated when using JNX_LOG macro]
-   *  @param format is a const string for formatting the appended arguments
-   *  @param ... and varadic args 
-   */
-  void jnx_log(jnx_log_config *config, const jnx_char *file, const jnx_char *function,const jnx_uint32 line,const jnx_char *format,...);
+  void jnx_log(jnx_int l, const jnx_char *file, 
+      const jnx_char *function, 
+      const jnx_uint32 line,const jnx_char *format,...);
 
-#define JNX_LOG_CONTEXT(X) \
-  jnx_log_config *X
+#define JNXLOG(LEVEL,FORMATTER, ...) jnx_log(LEVEL,__FILE__,__FUNCTION__,__LINE__,FORMATTER, ## __VA_ARGS__);
 
-  /**@fn JNX_LOG(CONFIG,FORMATTER,...)
-   * @brief a macro for logging that auto completes file,function,line information
-   * @param CONFIG is the context to use, can be DEFAULT_CONTEXT or NULL
-   * @param FORMATTER is the const jnx_char* formatter for displaying the subsequent args
-   * @param ... any optional arguments to be rendered by the formatter
-   */
-#ifndef DEBUG
-#define JNX_LOG(CONFIG,FORMATTER,...)
-#else
-#define JNX_LOG(CONFIG,FORMATTER, ...) jnx_log(CONFIG,__FILE__,__FUNCTION__,__LINE__,FORMATTER, ## __VA_ARGS__); 
-#endif
-  /**@fn JNX_LOG_CREATE(PATH,OUTPUT)
-   * @brief creates a log context
-   * @param PATH is an optional value for log output path when using OUTPUT=FILETYPE, can be NULL
-   * @param OUTPUT is the context output can be either FILETYPE or CONSOLETYPE, CONSOLETYPE can be NULL
-   * @return JNX_LOG_CONTEXT which is a macro surrounding jnx_log_config
-   */
-#define JNX_LOG_CREATE(PATH,OUTPUT) jnx_log_create(PATH,OUTPUT);
-  /**@fn JNX_LOG_DESTROY(X)
-   * @brief destroys the provided log context
-   * @param X is a pointer to pointer of log context 
-   */
-#define JNX_LOG_DESTROY(X) jnx_log_destroy(X);
-
+#define JNX_LOG(LEVEL,FORMATTER, ...) jnx_log(LEVEL,__FILE__,__FUNCTION__,__LINE__,FORMATTER, ## __VA_ARGS__);
 #ifdef __cplusplus
 }
 #endif
