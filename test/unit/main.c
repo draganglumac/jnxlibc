def369153241a67b3f5d3403c155be748d46aa5b
Dragan Glumac <dragan.glumac@gmail.com>
2015-12-20 18:24:54 +0000

    Fixing run_test_case signature.

--- test/unit/main.c
+++ test/unit/main.c
@@ -7,11 +7,11 @@
 #include "jnxlog.h"
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct {
   char *test_case_name;
-
-  void (*run_test_case)();
+  int (*run_test_case)();
 } test_case;
 
 void run_test_case(test_case tc) {
